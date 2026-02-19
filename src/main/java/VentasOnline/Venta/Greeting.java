81a819eeb0d3d0580156f4dfc8d3822a11101bb7
GezuzRomeo2 <gezuzromeo2@gmail.com>
2026-02-18 21:19:27 -0500

    primer push

---
+++ src/main/java/VentasOnline/Venta/Greeting.java
@@ -0,0 +1,16 @@
+package VentasOnline.Venta;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    private long id;
+    private String message;
+    private int age;
+    private String sex;
+
+}
---
+++ src/main/java/VentasOnline/Venta/GreetingController.java
@@ -0,0 +1,18 @@
+package VentasOnline.Venta;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+@RestController
+@RequestMapping(value = "greeting")
+public class GreetingController {
+    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello %s";
+    @GetMapping
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "age", defaultValue = "0") int age, @RequestParam(value = "sex", defaultValue = "?") String sex){
+        return new Greeting(counter.incrementAndGet(), String.format(template,name),age, sex);
+    }
+}
